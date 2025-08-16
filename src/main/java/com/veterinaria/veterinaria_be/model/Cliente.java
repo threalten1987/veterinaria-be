46662e1f04a1c3eb7c7c65c23067f31b27e6178b
threalten1987 <tarayamoreno@gmail.com>
2025-08-15 21:19:22 -0400

    Se deshabilitan las wildcards imports

--- src/main/java/com/veterinaria/veterinaria_be/model/Cliente.java
+++ src/main/java/com/veterinaria/veterinaria_be/model/Cliente.java
@@ -1,9 +1,12 @@
 package com.veterinaria.veterinaria_be.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "clientes")
--- src/main/java/com/veterinaria/veterinaria_be/model/Mascota.java
+++ src/main/java/com/veterinaria/veterinaria_be/model/Mascota.java
@@ -1,6 +1,14 @@
 package com.veterinaria.veterinaria_be.model;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "mascotas")
